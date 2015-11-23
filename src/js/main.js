2e69028e5d656aa0537c4aa17c8885141f4dc291
Will Franklin <will.franklin@guardian.co.uk>
2015-11-23 16:37:47 +0000

    Stuff

--- src/js/main.js
+++ src/js/main.js
@@ -27,7 +27,7 @@ spending.forEach(spend => {
     }
 });
 
-const width = 100, height = 50;
+const WIDTH = 100, HEIGHT = 50;
 
 var departments = Object.keys(departmentCosts)
     .filter(name => departmentCosts[name].cost > 0)
@@ -36,26 +36,53 @@ var departments = Object.keys(departmentCosts)
         return {
             name,
             'cost': department.cost,
-            'treemap': treemap(department.divisions, division => division.cost, width, height)
+            'treemap': treemap(department.divisions, division => division.cost, WIDTH, HEIGHT)
         };
     })
     .sort((a, b) => b.cost - a.cost);
 
-var governmentTreemap = treemap(departments, department => department.cost, width, height);
+var governmentTreemap = treemap(departments, department => department.cost, WIDTH, HEIGHT);
+
+function getBoxWidth(area) {
+    var height = Math.sqrt(area / (WIDTH  / HEIGHT));
+    return area / height;
+}
+
+function bound(v, min, max) {
+    return Math.max(min, Math.min(v, max));
+}
 
 export function init(el, context, config, mediator) {
     iframeMessenger.enableAutoResize();
 
+    //el.addEventListener('mousemove', evt => console.log(evt));
+
     departments.forEach((department, i) => {
         var box = governmentTreemap[i].box;
-        var area = box.width * box.height;
-        var width = Math.sqrt(area / 2);
-        console.log(area, box, width, area / width);
-
-        department.padding = 100 - area / width;
+        department.padding = WIDTH - getBoxWidth(box.width * box.height);
     });
 
     var sections = [{'name': 'Goverment overview', 'treemap': governmentTreemap}].concat(departments);
 
+    sections.forEach(section => {
+        //var cut = 30;
+        section.cut = getBoxWidth(WIDTH * HEIGHT * 10 / 100);
+    });
+
     el.innerHTML = templateFn({sections});
+
+    [].slice.apply(el.querySelectorAll('.js-section')).forEach(sectionEl => {
+        var cutEl = sectionEl.querySelector('.js-cut');
+        sectionEl.addEventListener('mousemove', evt => {
+            var rect = sectionEl.getBoundingClientRect();
+            var cutRect = cutEl.getBoundingClientRect();
+            var x = evt.clientX - rect.left,
+                y = evt.clientY - rect.top;
+            var left = bound(x - cutRect.width / 2, 0, rect.width - cutRect.width);
+            var top = bound(y - cutRect.height / 2, 0, rect.height - cutRect.height);
+
+            cutEl.style.left = (left / rect.width * 100) + '%';
+            cutEl.style.top = (top / rect.height * 100) + '%';
+        });
+    });
 }
